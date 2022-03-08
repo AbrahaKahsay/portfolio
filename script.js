3853c275bf6ad632e140a380a6ed902c6162998d
Abraha Kahsay <abrahak81@gmail.com>
2022-03-08 03:58:29 +0300

    Add js file to change mobile menu dynamicaly

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+//mobile menu
+// functions for changing the navbar or dropdown menu's style 
+
 function openNav() {
   document.getElementById('navBar').style.width = '100%';
 }
@@ -5,6 +10,8 @@ function openNav() {
 function closeNav() {
   document.getElementById('navBar').style.width = '0%';
 }
+
+// add event listner to open and close buttons.
 
 document.getElementById('hum').addEventListener('click', () => {
   openNav();
