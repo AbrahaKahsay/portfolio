7aa2e0a4468908adf456c700fdc7312e2452114b
Abraha Kahsay <abrahak81@gmail.com>
2022-03-12 12:27:18 +0300

    Create cards dynamically with DOM

--- script.js
+++ script.js
@@ -24,3 +24,50 @@ document.getElementById('closebtn').addEventListener('click', () => {
 document.querySelectorAll('.links').forEach((n) => n.addEventListener('click', () => {
   document.getElementById('navBar').style.width = '0%';
 }));
+
+
+// creating cards Dynamically using DOM 
+
+let workSection = document.querySelector('container-works-section');
+let counter = 0;
+// create an array of objects to store the cards
+const data = [
+  {
+      //  first card 
+    title: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image: "images/Snapshoot-Portfolio(1).svg",
+    technologies: ["html", "css", "javascript"],
+    live: "images/Icon-Export.svg",
+    source: "images/Icon-gitHub.png"
+
+  },
+  {
+    // second card
+    title: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image: "images/Snapshoot-Portfolio(2).svg",
+    technologies: ["html", "css", "javascript"],
+    live: "images/Icon-Export.svg",
+    source: "images/Icon-gitHub.png"
+  },
+  {
+    // third card
+    title: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image: "images/Snapshoot-Portfolio(3).svg",
+    technologies: ["html", "css", "javascript"],
+    live: "images/Icon-Export.svg",
+    source: "images/Icon-gitHub.png"
+
+  },
+  {
+    // fourth card
+    title: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image: "images/Snapshoot-Portfolio(4).svg",
+    technologies: ["html", "css", "javascript"],
+    live: "images/Icon-Export.svg",
+    source: "images/Icon-gitHub.png"
+  }
+]
